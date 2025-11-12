b526e1e90d31f8a292dea5831c038e9aa0b6555c
kavinda1996 <gayashankavinda95@gmail.com>
2025-11-12 16:32:21 +0530

    creating suppliers form

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -33,7 +33,15 @@ public class DashBoardController {
         loadFormContent.getChildren().add(load);
     }
 
-    public void btnSupplierFormOnAction(ActionEvent actionEvent) {
+    public void btnSupplierFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/suppliers_form.fxml");
+
+        assert resource != null;
+
+        Parent load = FXMLLoader.load(resource);
+
+        loadFormContent.getChildren().clear();
+        loadFormContent.getChildren().add(load);
     }
 
     public void btnReportsFormOnAction(ActionEvent actionEvent) {
--- src/main/java/controller/SuppliersController.java
+++ src/main/java/controller/SuppliersController.java
@@ -1,4 +1,101 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class SuppliersController {
+
+    @FXML
+    private Button btnAddSupplier;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnDeleteSupplier;
+
+    @FXML
+    private Button btnFilterByName;
+
+    @FXML
+    private Button btnFilterBySupplier;
+
+    @FXML
+    private Button btnRedo;
+
+    @FXML
+    private Button btnUndo1;
+
+    @FXML
+    private Button btnUndo2;
+
+    @FXML
+    private TableColumn<?, ?> colBrand;
+
+    @FXML
+    private TableColumn<?, ?> colExpiryDate;
+
+    @FXML
+    private TableColumn<?, ?> colMedicineID;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colSellingPrice;
+
+    @FXML
+    private TableColumn<?, ?> colSupplier;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnAddSupplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteSupplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnFilterByNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnFilterBySupplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRedoOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUndo1OnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUndo2OnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
 }
