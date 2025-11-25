417cb492c0c85ff9e1e136296439cee054864334
kavinda1996 <gayashankavinda95@gmail.com>
2025-11-26 05:28:33 +0530

    purchase history table create

--- src/main/java/controller/PurchaseHistoryController.java
+++ src/main/java/controller/PurchaseHistoryController.java
@@ -1,17 +1,26 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.PurchaseHistory;
+import model.dto.PurchaseMedicines;
+import util.DBConnection;
 
+import java.net.URL;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class PurchaseHistoryController {
+public class PurchaseHistoryController implements Initializable {
 
     public TableColumn colSupplierName;
+    public TableView <PurchaseHistory> tblPurchaseHistory;
     @FXML
     private Button btnDeleteRecord;
 
@@ -59,6 +68,48 @@ public class PurchaseHistoryController {
     @FXML
     void txtSearchOnAction(ActionEvent event) {
 
+    }
+    static ObservableList<PurchaseHistory> purchaseList = FXCollections.observableArrayList();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colInvoiceNo.setCellValueFactory(new PropertyValueFactory<>("invoiceNo"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        colSupplierName.setCellValueFactory(new PropertyValueFactory<>("supplierName"));
+        colTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+        colPaymentType.setCellValueFactory(new PropertyValueFactory<>("paymentType"));
+
+        tblPurchaseHistory.setItems(purchaseList);
+        loadTable();
+
+        lblDate.setText(LocalDate.now().toString());
+    }
+
+
+    public void loadTable() {
+        purchaseList.clear();
+
+        try {
+            Connection con = DBConnection.getInstance();
+            String sql = "SELECT * FROM purchase_history";
+            ResultSet rs = con.prepareStatement(sql).executeQuery();
+
+            while (rs.next()) {
+
+                purchaseList.add(new PurchaseHistory(
+                        rs.getInt("invoice_no"),
+                        rs.getDate("date").toLocalDate(),
+                        rs.getString("supplier_name"),
+                        rs.getDouble("total"),
+                        rs.getString("payment_type")
+
+                ));
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/model/dto/PurchaseHistory.java
+++ src/main/java/model/dto/PurchaseHistory.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class PurchaseHistory {
     private int invoiceNo;
     private LocalDate date;
-    private String customerName;
+    private String supplierName;
     private double total;
     private String paymentType;
 }
